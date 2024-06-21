d2b64a78699e89d6a978ef9e6f7ce517866644a4
meshack <meshackkimaiyo5@gmail.com>
2024-06-21 11:03:21 +0300

    categpory items

--- src/routes/RentRoute.ts
+++ src/routes/RentRoute.ts
@@ -3,6 +3,18 @@ import {param} from "express-validator"
 import RentController from "../controllers/RentController"
 
 const router = express.Router()
+router.get("/:rentId",
+  param("rentId")
+.isString()
+.trim()
+.notEmpty()
+.withMessage("RentId  parameter must be a valid string"),
+RentController.getRent
+
+);
+
+
+
 
 router.get(
     "/search/:city",
